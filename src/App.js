948fa1f0bb147d0f2439f6bcbbc4a3c09a276140
mrbeastmahmoud <mahmoudsu22@gmail.com>
2022-05-08 00:10:05 +0300

    add Meals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Layout/Header';
-import { Fragment } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Layout/Header";
+import { Fragment } from "react";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
-     <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
